fea7d079b78944b693a8516f2282b13737de7f29
Xafiq <148711588+Xafiq@users.noreply.github.com>
2024-09-17 14:10:31 +0200

    Update app.py

--- currency-converter/convertisseur-devises-1/app.py
+++ currency-converter/convertisseur-devises-1/app.py
@@ -3,7 +3,7 @@ from PySide2 import QtWidgets, QtGui, QtCore
 class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Convertisseur de devises")
+        self.setWindowTitle("currency converter")
 
 app = QtWidgets.QApplication([])
 win = App()
