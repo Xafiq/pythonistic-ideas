b504fa968597dca5f01957dbdf5eb7b7244ee521
Xafiq <148711588+Xafiq@users.noreply.github.com>
2024-09-17 14:14:23 +0200

    Update app.py

--- currency-converter/convertisseur-devises-2/app.py
+++ currency-converter/convertisseur-devises-2/app.py
@@ -3,22 +3,22 @@ from PySide2 import QtWidgets, QtGui, QtCore
 class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Convertisseur de devises")
+        self.setWindowTitle("currency converter")
         self.setup_ui()
 
     def setup_ui(self):
         self.layout = QtWidgets.QHBoxLayout(self)
-        self.cbb_devisesFrom = QtWidgets.QComboBox()
-        self.le_montant = QtWidgets.QSpinBox()
-        self.cbb_devisesTo = QtWidgets.QComboBox()
-        self.le_montantConverti = QtWidgets.QSpinBox()
-        self.btn_inverser = QtWidgets.QPushButton("Inverser devises")
+        self.cbb_currencyFrom = QtWidgets.QComboBox()
+        self.amount = QtWidgets.QSpinBox()
+        self.cbb_currencyTo = QtWidgets.QComboBox()
+        self.le_ConvertedAmount = QtWidgets.QSpinBox()
+        self.btn_reverse = QtWidgets.QPushButton("Reverse currencies")
 
-        self.layout.addWidget(self.cbb_devisesFrom)
-        self.layout.addWidget(self.le_montant)
-        self.layout.addWidget(self.cbb_devisesTo)
-        self.layout.addWidget(self.le_montantConverti)
-        self.layout.addWidget(self.btn_inverser)
+        self.layout.addWidget(self.cbb_currencyFrom)
+        self.layout.addWidget(self.amount)
+        self.layout.addWidget(self.cbb_currencyTo)
+        self.layout.addWidget(self.ConvertedAmount)
+        self.layout.addWidget(self.btn_reverse)
   
 app = QtWidgets.QApplication([])
 win = App()
